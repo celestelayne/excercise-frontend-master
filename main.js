7d38b2f2f000b273edb3008a07dd7a6b9c182a30
Celeste Layne <layne.celeste@gmail.com>
2016-02-27 12:20:09 -0800

    html5 history api

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 
 var shows = [];
 
+// get the data
 function getShows(){
 	$.ajax({
 		method: 'GET',
@@ -29,19 +30,27 @@ function getShows(){
 					title: title
 				});
 			}
-			console.log(shows);
+			// console.log(shows);
 			renderShows(shows);
 		},
 	    error: function(){
       console.log('sad face');
     }
 	});
+
 }
 
 // Structure
 // ---------------------------------------------
+
+	// get references to page elements
 	var thumbnail = document.querySelector('.footer-list');
-	var singleShow = document.querySelector('.single-show');
+	var contentElement = document.querySelector('.single-show');
+
+	// grab each thumbnail
+	var thumbnailElements = document.querySelectorAll('.show-selector');
+	var titleEl = document.querySelector('.title');
+	var imageEl = document.querySelector('.show-image');
 
 	var img = document.createElement('img');
 	var li = document.createElement('li');
@@ -53,11 +62,7 @@ function getShows(){
 // Event Listeners
 // ---------------------------------------------
 	
-	window.addEventListener('popstate', function(event){
-		var state = event.state;
-		console.log(event);
-	});
-
+	thumbnail.addEventListener('click', getPageData);
 	thumbnail.addEventListener('click', toggleShow);
 
 // Event Handler Functions
@@ -67,26 +72,38 @@ function getShows(){
 		thumbnailPressed = event.target.src;
 		// returns img
 		imagePath = event.srcElement.attributes.src.value;
-		console.log(imagePath);
-		console.log(shows);
+		// console.log(imagePath);
+		// console.log(shows);
 
 		img.setAttribute('src', thumbnailPressed);
 
+		h2.classList.add('title');
+		img.classList.add('show-image');
+
 		createImage();
+		// updateContent()
+
 	}
+
+	// Functions
+	// ---------------------------------------------
 
 	function createImage(){
 		console.log(shows);
-		var currentUrl = window.location.href;
+		var currentUrl = thumbnailPressed;
 
 		// var currentUrl = window.location.href;
-		// console.log(currentUrl);
-		// li.setAttribute('data', currentUrl);
+		console.log(currentUrl);
 
-		// shows.forEach(function(show){
-		// 	console.log(show.image);
+		// data-url ==> img src (event.target.src) <== thumbnailPressed
+		// var showURL = li.setAttribute('data-url', currentUrl);
 
-		var page_url = li.setAttribute('data-url', currentUrl);
+		var pageData = currentUrl.split('.')[0];
+		console.log(pageData);
+
+		// Creates a new history entry.
+		// window.history.pushState(stateObj, title, URL);
+		// window.history.pushState(pageData, showURL);
 
 		shows.forEach(function(show){
 			// console.log(show.image);
@@ -104,21 +121,78 @@ function getShows(){
 				console.log('false');
 			}
 		});
-
+		
 		// insert clicked image into DOM
 		li.appendChild(img);
 		li.appendChild(h5);
 		li.appendChild(h2);
 
-		singleShow.appendChild(li);
+		contentElement.appendChild(li);
+
+		// updateContent(pageData);
+
+	}
+
+	function updateContent(stateObj){
+	// 	// get page elements
+	// 	var titleEl = document.querySelector('.title');
+	// 	var episodeEl = document.querySelector('.episode');
+
+		console.log(stateObj);
+	// 	// check to make sure the state isn't null
+		if(stateObj){
+			document.title = stateObj.title;
+			titleEl.innerHTML = stateObj.title;
+			iamgeEl.innerHTML = stateObj.episode;
+		}
+	};
+
+	function getPageData(event){
+			console.log('in here');
+		for (var i = 0; i < thumbnailElements.length; i++) {
+			var check = thumbnailElements[i];
+			console.log(check)
+		}
+
+					// event.preventDefault();
+
+					// var showURL = this.attributes;
+					// console.log(showURL)
+
+			
+
+
+			
+			// console.log(showURL)
+			// 	var showData = shows[showURL.split('.')[0]];
+			// 	console.log(showData)
+
+					// Update the title and content.
+		      // updateContent(showData);
+
+		      // Create a new history item.
+		      // history.pushState(showData, showData.title, showURL);
+
+			// updateContent();
 	}
 
 
+	// Update the page content when the popstate event is called.
+	window.addEventListener('popstate', function(event) {
+	  updateContent(event.state)
+	});
+
+	// Load initial content.
+	updateContent(shows[0]);
+
+  // Update this history event so that the state object contains the data
+  // for the homepage.
+  // history.replaceState(shows[0], shows[0]['id'], '');
 
 // Handlebars
 // ---------------------------------------------
 	function renderShows(show){
-		console.log('rendering shows: ', show);
+		// console.log('rendering shows: ', show);
 
 	  // handlebars template
 	  // get the template content
--- script.js
+++ script.js
@@ -1,61 +1,131 @@
-console.log('Sanity Check: main.js is working!');
+window.onload = function() {
+  console.log('Sanity Check: main.js is working!');
 
-  var shows = [
-	  {
-	    "id": 1,
-	    "title": "Gaycation",
-	    "episodes": 12,
-	    "product_image_url": "/images/show_1.png"
-	  },
-	  {
-	    "id": 2,
-	    "title": "Fuck, That's Delicious",
-	    "episodes": 24,
-	    "product_image_url": "/images/show_2.png"
-	  },
-	  {
-	    "id": 3,
-	    "title": "WEEDIQUETTE",
-	    "episodes": 15,
-	    "product_image_url": "/images/show_3.png"
-	  },
-	  {
-	    "id": 4,
-	    "title": "The Business of Life",
-	    "episodes": 5,
-	    "product_image_url": "/images/show_4.png"
-	  }
-	]
+  getShows(shows);
 
-	shows.forEach(getShows);
+};
 
+var shows = [];
+var imagePath;
+
+// get the data
+function getShows(){
+	$.ajax({
+		method: 'GET',
+		url: 'https://api.myjson.com/bins/3rsrp',
+		dataType: 'json',
+		success: function(data) {
+			for (var i = 0; i < data.length; i++){
+				var showData = data[i];
+				// console.log(showData);
+				var id = showData.id;
+				var episode = showData.episodes;
+				var image = showData.product_image_url.slice(1);
+				// console.log(image);
+				var title = showData.title;
+
+				shows.push({
+					id: id,
+					episode: episode,
+					image: image,
+					title: title
+				});
+			}
+			console.log(shows);
+			renderShows(shows);
+			updateContent(shows);
+			createHistoryItem(shows);
+		},
+	    error: function(){
+      console.log('sad face');
+    }
+	});
+
+}
+
+  // Create page elements
+ 	var img = document.createElement('img');
+	var li = document.createElement('li');
+	var h2 = document.createElement('h2');
+	var h5 = document.createElement('h5');
+
+	// Get references to the page elements.
+	var thumbnail = document.querySelector('.footer-list');
+  
+	var navLinks = document.querySelectorAll('.show-selector');
+	console.log(navLinks)
+  // var imageElement = document.querySelector('img');
+  var titleElement = document.querySelector('h2');
+  var episodeElement = document.querySelector('h5'); 
+
+// Event Listeners
+// ---------------------------------------------
 	
+	thumbnail.addEventListener('click', updateContent);
 
-	
-	
-	var p = document.createElement('p');
-
-
-	function getShows(show){
-		// console.log(show);
-		var img = document.createElement('img');
-
-		var title = show.title;
-		var episode = show.episode;
-
-		var image = show.product_image_url.slice(1);
-		img.setAttribute('src', image);
-
-		// console.log(image);
-		renderShowToFooter(show)
+	for(var i = 0; i < navLinks.length; i++){
+		navLinks[i].addEventListener('click', createHistoryItem);
 	}
 
-	function renderShowToFooter(show){
-		var thumbnail = document.querySelector('.footer-list');
-		var li = document.createElement('li');
-		var img = document.createElement('img');
-		var image = show.product_image_url.slice(1);
-		img.setAttribute('src', image);
-		thumbnail.appendChild(li);
-		li.appendChild(img);
+	// Event Handler Functions
+	// ------------------------------------------
+
+
+  // Update the page content.
+  function updateContent(stateObj) {
+  	console.log(stateObj);
+  // Check to make sure that this state object is not null.
+    if (stateObj) {
+      document.title = stateObj.title;
+      // imageElement.innerHTML = stateObj.
+      titleElement.innerHTML = stateObj.title;
+      episodeElement.innerHTML = stateObj.episodes;
+    }
+  };
+
+	// i think this does not work becasue
+	// the elements are create by handlebars
+	// how to fix?
+	function createHistoryItem(event){
+		// event.preventDefault();
+		console.log("inside here")
+		var currentURL = location.pathname;
+		var pageData = currentURL.split('.')[0];
+		console.log(pageData)
+			// Fetch the page data using the URL in the link.
+			this.shows.forEach(function(show){
+				var pageURL = show.id;
+				var url = pageData + "?id=" + pageURL;
+				console.log(url);
+			});
+		  // Update the page content when the popstate event is called.
+			window.addEventListener('popstate', function(event) {
+				console.log('popstate fired!');
+		  	updateContent(event.state)
+			});
+
+		  // Load initial content.
+		  console.log(shows);
+	}
+
+
+
+
+
+// Handlebars
+// ---------------------------------------------
+	function renderShows(show){
+		// console.log('rendering shows: ', show);
+
+	  // handlebars template
+	  // get the template content
+	  var template = $('#showTemplate').html();
+
+	  // compile the template data into a function
+	    var compiledTemplate = Handlebars.compile(template);
+	  // Render the data into the template
+	    var htmlFromCompiledTemplate = compiledTemplate({ show: show });
+
+	  // Overwrite the contents of #arts with rendered html back to document
+	    $('#shows').append( htmlFromCompiledTemplate );
 	}
